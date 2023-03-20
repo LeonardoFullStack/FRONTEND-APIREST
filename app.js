603dd2b438f593515b1e971c89c6d1678231632a
esteveleonardo@gmail.com <esteveleonardo@gmail.com>
2023-03-20 23:33:55 +0100

    Verificación con jwt completada

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const port = 3005
+const port = 4004
 /* const {validar} = require('./controllers/frontControllers') */
 
 
