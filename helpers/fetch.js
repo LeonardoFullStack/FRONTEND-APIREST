be4c8cf39f9bd4d1197a4a4201e879895e01137d
leonardo <esteveleonardo@gmail.com>
2023-07-17 20:29:37 +0200

    back conectado

--- helpers/fetch.js
+++ helpers/fetch.js
@@ -1,4 +1,4 @@
-const urlBase = 'http://localhost:4001/api/v1'
+const urlBase = 'https://api-proyecto-975q.onrender.com/api/v1'
 const {fetch} = 'node-fetch'
 //http://localhost:4001/api/v1
 //https://api-proyecto-975q.onrender.com/api/v1
